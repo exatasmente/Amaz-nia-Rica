64c5df529e08268ac86928988da525387a9abc4c
Luiz Neto <lu_u_z@hotmail.com>
2018-01-12 09:20:49 -0300

    V 0.0.0.6.2

--- src/providers/woo/woo.ts
+++ src/providers/woo/woo.ts
@@ -1,16 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as WC from 'woocommerce-api';
-/*
-  Generated class for the WooProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class WooProvider {
   wooCommerce : any;
-  constructor(public http: HttpClient) {
+  constructor() {
     this.wooCommerce = WC({
       url:"http://amazoniaricaapi.000webhostapp.com/",
       consumerKey:"ck_e5127b8d77116b6a7b28bad8fc4134a2bf09348d",
